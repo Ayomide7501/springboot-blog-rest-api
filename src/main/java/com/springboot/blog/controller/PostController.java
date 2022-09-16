92a8cf7a99cbf3203b724c104b8016910458d067
Ayomide Yusuff <Xquizit52@gmail.com>
2022-09-16 06:31:59 +0100

    added url path versioning to post controller

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostDtoV2;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
@@ -10,10 +11,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping()
 public class PostController {
     private PostService postService;
 
@@ -23,13 +25,13 @@ public class PostController {
 
     @PreAuthorize("hasRole('ADMIN')")
     //create blog post
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
     //get all posts rest api
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -40,14 +42,30 @@ public class PostController {
     }
 
     //get post by id
-    @GetMapping("/{id}")
-    public  ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
+    @GetMapping("/api/v1/posts/{id}")
+    public  ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
+    }
+    @GetMapping("/api/v2/posts/{id}")
+    public  ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id){
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        List<String> tags = new ArrayList<>();
+        tags.add("java");
+        tags.add("Spring boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+        postDtoV2.setComments(postDto.getComments());
+        return ResponseEntity.ok(postDtoV2);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     //update post by id rest api
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public  ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
      PostDto postResponse = postService.updatePost(postDto, id);
 
@@ -56,7 +74,7 @@ public class PostController {
 
     @PreAuthorize("hasRole('ADMIN')")
     //delete post rest api
-    @DeleteMapping("/{id}")
+    @DeleteMapping("//api/v1/posts{id}")
     public ResponseEntity<String>deletePost(@PathVariable(name = "id") long id){
         postService.deletePostById(id);
         return new ResponseEntity<>("Post deleted successfully.", HttpStatus.OK);
